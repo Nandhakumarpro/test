2357eac251e7071c4d6865976bb24e55805fc910
Nandhakumarpro <41862601+Nandhakumarpro@users.noreply.github.com>
2020-02-09 18:47:23 +0530

    Update traveltimeByWifi.py

--- traveltimeByWifi.py
+++ traveltimeByWifi.py
@@ -10,17 +10,6 @@ import mysql.connector as msc
 cur = conn.cursor()
 LOCALS = False
 from multiprocessing import  Pool
-dixt = {  }
-
-allDevices =   [ "104" ,"105" , "902" , "903","102" , "103" ,"901" , "113" ]
-devicePaired = [
-  ["104" , "105" , "000M1D" , "000M1U"],
-  ["902" ,"903" , "0019BU" , "0019BD" ],
-  ["102" , "103" , "0005AD" , "0005AU"],
-  ["901" , "104" , "00M70D" , "00M70U"],
-  ["902" , "113" , "00519D" , "00519U"],
-  ["901" , "105" , "00570U" , "00570D"]
-]
 
 
 class wifiProject :
@@ -36,14 +25,12 @@ class wifiProject :
         with open('data1_'+devicePairEach+'.json', 'w') as outfile :
             json.dump(q_cond, outfile)
 
-        # query_aws = '~/Music/pypy3.5-v7.0.0-linux64/bin/aws dynamodb query --table-name WMS_COEUT --key-condition-expression "device_id = :did and mac_timestamp BETWEEN :st AND :et" --expression-attribute-values file://data1_'+ devicePairEach+'.json > ' +\
-        #             self.filePath+ '/temp_' + devicePairEach + '.json'
         query_aws = 'aws dynamodb query --table-name WMS_COEUT --key-condition-expression "device_id = :did and mac_timestamp BETWEEN :st AND :et" --expression-attribute-values file://data1_' + devicePairEach + '.json > ' + \
                     self.filePath + '/temp_' + devicePairEach + '.json'
 
         print(query_aws)
         os.system( query_aws )
-        #if LOCALS:dixt['prw'] = locals()
+       
 
     def read_wms(self , allDevices , timeIndex  ):
 
@@ -57,19 +44,14 @@ class wifiProject :
         if LOCALS:dixt['rms'] = locals()
         #self.dataProcess()
     def mainExecutor(self  , liveTime  ):
-       # Devices =
-        #for index in range( 1556476200 , 1557081000, 60 )[50:]:
+
         self.read_wms( allDevices, liveTime )
         self.dataProcess(allDevices)
         for pair in devicePaired:
             self.csvUpdate( pair )
-                #print(pair)
-                
-        #return self.time_now
 
     def dataProcess(self , allDevices ) :
-        #self.D = devicePairedEach
-        #self.createDbIfNotAvail(devicePairedEach)
+
         self.createDbIfNotAvail(allDevices)
 
         mappedPath = lambda x : [ self.filePath +'/' +'temp_'+x+'.json' ,x]
@@ -77,12 +59,6 @@ class wifiProject :
         for each in list(map(mappedPath , allDevices)) :
             self.excelCreate( each )
 
-        """
-        file1path = self.filePath+'/'+'temp_'+self.D[0]+'.json'
-        file2path = self.filePath+'/'+'temp_'+self.D[1]+'.json'
-        if LOCALS : dixt['dp'] = locals()
-        #lwrt(str(locals()))"""
-        #self.excelCreate( file1path , file2path )
     def excelCreate(self , file1path ):
         df1 = self.dataFrameGen(file1path[0])
         #df2 = self.dataFrameGen(file2path)
@@ -103,19 +79,11 @@ class wifiProject :
     def filterGroupsSub ( self , subDF  ):
         df = subDF
         df = df.sort_values( 'logtime' , ascending= False)
-        """
-        y = 100000000000
 
-        for key, Frame in df['mac'].iteritems():
-            temp = int(float(df.loc[key, 'logtime']))
-            df.loc[key, 'Duplicates'] = [True if y - 30 > temp else None][0]
-            y = temp
-        df = df.dropna().iloc[: , :-1]"""
         return df.iloc[0:1 ]
 
     def ConfigureAndExe(self ,Groups1  , d ):
         Keys1 = Groups1.groups.keys()  #
-        #Keys2 = Groups2.groups.keys()  #
         group1DF = pd.DataFrame(columns=wifiProject.dataFrameColumns())
         #group2DF = pd.DataFrame(columns=wifiProject.dataFrameColumns())
         p = Pool(processes=3)
@@ -131,18 +99,10 @@ class wifiProject :
             group1DF = group1DF.append(self.df1List)
         except:
             print('dataframe1 empty')
-        """
-        try:
-            group2DF = group2DF.append(self.df2List)
-        except:
-            print('dataframe2 empty')
-        """
+
         self.updateDBLeft(group1DF , d )
-        #self.updateDBRight(group2DF)
-        #p.map(self.updateDBLeft ,  self.df1List )
-        #p.map( self.updateDBRight , self.df2List)
-        #p.close()
         print('second pool closed')
+        
     def csvUpdate(self , devicePairedEach ):
         self.D = devicePairedEach
         resultQuery1 = wifiProject.resultQuery1(self.D[0] , self.D[1] , self.time_now , self.time_past)
@@ -172,9 +132,6 @@ class wifiProject :
         path = wifiProject.csvFilePath()
         left1 = path + f"{self.D[2]}-"+datetime.strftime(datetime.fromtimestamp(self.time_now) , "%d-%m-%y")+".csv"
         right1 = path + f"{self.D[3]}-" + datetime.strftime(datetime.fromtimestamp(self.time_now) , "%d-%m-%y") + ".csv"
-        #print(left1)
-        #print(right1)
-
         try:
             if os.path.isfile(left1):
                 #print(groups.get_group(self.D[2]))
@@ -192,34 +149,13 @@ class wifiProject :
 
         except:
             print('None')
-        # wifiProject.csvSqlUpdate( groups.get_group(self.D[2]) , self.D[2])
-        # wifiProject.csvSqlUpdate(groups.get_group( self.D[3]) , self.D[3])
+
         conn.commit()
-        #print(data)
-        #conn.close()
-        #resultDf.to_excel( dataPath + '/Results/result'+self.count+'.xls' , sheet_name='sheet1')
+    
     def updateDBLeft ( self , df , d ) :
         for each in df.index :
             temp = df.loc[each]
             sqlQuery = wifiProject.updateQuery(d) +S(temp.device_id)+','+S(temp.mac)+','+temp.logtime+','+temp.mac_timestamp+" , "+S(f'{datetime.strftime(datetime.fromtimestamp(float(temp.logtime)) , "%d-%m,%H:%M:%S")}')+")"
-            #print(sqlQuery)
             cur.execute(sqlQuery)
         conn.commit()
-    def updateDBRight(self , df ):
-        for each in df.index :
-            temp = df.loc[each]
 
-            sqlQuery = wifiProject.updateQuery(self.D[1]) +S(temp.device_id)+','+S(temp.mac)+','+temp.logtime+','+temp.mac_timestamp+ " , " +S(f'{datetime.strftime(datetime.fromtimestamp(float(temp.logtime)) , "%d-%m,%H:%M:%S")}')+')'
-            #print(sqlQuery)
-            cur.execute(sqlQuery)
-        conn.commit()
-    def printMethod(func ):
-        def wrapper( *args , **kwargs ):
-            y = func(*args , **kwargs )
-            print(y)
-            return y
-        return wrapper
-    def filePath():
-        path1 = ['..','supportFiles' , 'Device']
-        path2 = [ ".." , 'tempFiles' , 'Device']
-        return '/'.join( path1 
